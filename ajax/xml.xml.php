ffcf63167b7e9a06d70c64035989e113ddb7774b
CupBlack <deltaalfalambda@gmail.com>
2017-11-11 06:48:12 -0200

    Salvando projeto

---
+++ ajax/xml.xml.php
@@ -0,0 +1,9 @@
+<?php
+header('access-control-allow-origin: *');
+header("Content-type: text/xml");
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <id>1</id>
+  <pontos>456</pontos>
+</root>
---
+++ user.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ */
+class user
+{
+  public $id;
+  public $nick;
+  public $senha;
+  function __construct()
+  {
+    # code...
+  }
+  static function autenticar($user,$senha){
+
+  }
+}
